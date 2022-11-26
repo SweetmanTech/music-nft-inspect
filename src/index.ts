23aa6c72a8392e7a4f490dff59585e608b2f8be2
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-26 10:27:50 -0300

    I refactor catalog-20220222 evaluator.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { evaluator as evaluateCatalog20220202 } from "./evaluators/catalog-20220222";
+import { evaluate as evaluateCatalog20220202 } from "./evaluators/catalog-20220222";
+
+const evaluate = (metadata: any) => {
+  const musicMetadata = evaluateCatalog20220202(metadata)
+  const parsedScores = [{...musicMetadata, name: "Catalog-20220222"}]
+  return parsedScores
+
+}
 
 export {
+  evaluate,
   evaluateCatalog20220202,
 };
