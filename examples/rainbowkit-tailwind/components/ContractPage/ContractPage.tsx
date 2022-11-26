9b20fa7b9c2f4242716b880a12f85b19ad507972
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-26 10:26:40 -0300

    I init <ContractPage /> component.

---
+++ examples/rainbowkit-tailwind/components/ContractPage/ContractPage.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { evaluate } from "../../../../dist"
+import FocusSection from "../FocusSection"
+import ResultsTable from "../ResultsTable"
+import { LensShareButton } from "@infinity-keys/react-lens-share-button";
+import '@infinity-keys/react-lens-share-button/dist/style.css'
+
+const ContractPage = (props: any) => {
+    const [focus, setFocus] = useState();
+    const {metadata, chainId, contractAddress} = props
+    console.log("contractAddress", contractAddress)
+    const results = evaluate(metadata)
+    return (
+        <div>
+            <ResultsTable setFocus={setFocus} results={results}/>
+            <FocusSection focus={focus} />
+            <LensShareButton 
+                postBody={`music nft score: 98.6%`}
+                url={`https://musicnftinspect.xyz/${chainId}/${contractAddress}`}
+                preview={true}
+            />
+        </div>
+    )
+}
+
+export default ContractPage
