89c5155881d02d376096b40dc1de94f4af89ca10
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-26 12:19:58 -0300

    I address losingmyego feedback: disabled state for the button until info is filled in

--- examples/rainbowkit-tailwind/components/SubmitButton/SubmitButton.tsx
+++ examples/rainbowkit-tailwind/components/SubmitButton/SubmitButton.tsx
@@ -3,8 +3,11 @@ import {evaluate} from "music-nft-inspect"
 
 const SubmitButton = (props: any) => {
     const {metadata, onSuccess} = props
-    const className = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    const backgroundColor = metadata.length > 0 ? `focus:ring-4 focus:ring-blue-300 bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:focus:ring-blue-800 dark:hover:bg-blue-700` : "bg-blue-200 hover:bg-blue-200 dark:bg-blue-200"
+    const className = `text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none ${backgroundColor}`
     const buttonText = "Submit";
+
+    console.log("METADATA", metadata)
 
     const handleClick = () => {
         let metadataJson = {};
@@ -19,7 +22,7 @@ const SubmitButton = (props: any) => {
     }
 
     return (
-        <button type="button" className={className} onClick={handleClick}>{buttonText}</button>
+        <button type="button" disabled={metadata.length === 0} className={className} onClick={handleClick}>{buttonText}</button>
     )
 }
 
