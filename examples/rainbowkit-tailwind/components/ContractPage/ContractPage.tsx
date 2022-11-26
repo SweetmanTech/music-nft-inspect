a504ba253a87c9380b6311c25160cab409ffa3d4
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-26 12:09:11 -0300

    I tweak design

--- examples/rainbowkit-tailwind/components/ContractPage/ContractPage.tsx
+++ examples/rainbowkit-tailwind/components/ContractPage/ContractPage.tsx
@@ -11,14 +11,28 @@ const ContractPage = (props: any) => {
     const results = evaluate(metadata)
 
     return (
-        <div>
-            <ResultsTable setFocus={setFocus} results={results}/>
-            <FocusSection focus={focus} />
-            <LensShareButton 
-                postBody={`music nft score: 98.6%`}
-                url={`https://musicnftinspect.xyz/${chainId}/${contractAddress}`}
-                preview={true}
-            />
+        <div className="flex flex-col gap-4 grid-cols-1 w-full pt-5">
+            <h1 className="text-center text-4xl text-[#0d76fc] p-5">
+                <a rel="noreferrer" target="_blank" href="https://github.com/SweetmanTech/music-nft-inspect">
+                    music nft inspect
+                </a>
+            </h1>
+            <div className="flex grid justify-around grid-cols-5 p-3">
+                <div className="col-span-5 lg:col-span-2">
+                    <div>Contract: {contractAddress}</div>
+                    <LensShareButton 
+                        postBody={`music nft score: 98.6%`}
+                        url={`https://musicnftinspect.xyz/${chainId}/${contractAddress}`}
+                        preview={true}
+                    />
+                    <p className="text-xl">Metadata</p>
+                    {JSON.stringify(metadata)}
+                </div>
+                <div className="text-center border border-500-red col-span-5 lg:col-span-3">
+                    <ResultsTable setFocus={setFocus} results={results}/>
+                    <FocusSection focus={focus} />
+                </div>
+            </div>
         </div>
     )
 }
