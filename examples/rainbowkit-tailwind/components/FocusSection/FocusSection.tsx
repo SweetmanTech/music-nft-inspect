b80fd80a1fd66891c25e3778cfab8e6008247429
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-27 09:20:12 -0300

    I rollback FocusSection changes.

--- examples/rainbowkit-tailwind/components/FocusSection/FocusSection.tsx
+++ examples/rainbowkit-tailwind/components/FocusSection/FocusSection.tsx
@@ -1,8 +1,6 @@
 const FocusSection = (props: any) => {
     const {focus} = props
-    const {matching, missing} = focus
-    console.log("MATCHING", matching)
-    console.log("missing", missing)
+
     return (
         <div>
             {focus ? focus.map((item: string) => <p key={item}>{item}</p>) : ""}
