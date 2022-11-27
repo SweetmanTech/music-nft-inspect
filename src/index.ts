da005ebf37adb59b0be7b537c09e685fa5eeafee
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-27 07:30:44 -0300

    I export manifold functions in npm package.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { evaluate as evaluateCatalog20220202 } from "./evaluators/catalog-20220222";
 import { evaluate as evaluateZora20221126 } from "./evaluators/zora-20221126";
+import { evaluate as evaluateManifold20221127 } from "./evaluators/manifold-20221127";
 import {index as indexDecent20221126} from "./indexers/decent-20221126";
 
 const evaluate = (metadata: any) => {
   const musicMetadata = evaluateCatalog20220202(metadata)
   const zoraScore = evaluateZora20221126(metadata)
+  const manifoldScore = evaluateManifold20221127(metadata)
   const parsedScores = [
     {...musicMetadata, name: "Music Metadata Standard"}, 
-    {...zoraScore, name: "Zora Creator"}
+    {...zoraScore, name: "Zora Creator"},
+    {...manifoldScore, name: "Manifold"}
   ]
   return parsedScores
 }
@@ -22,5 +25,6 @@ export {
   indexDecent20221126,
   evaluate,
   evaluateCatalog20220202,
+  evaluateManifold20221127,
   evaluateZora20221126
 };
